ad863b504ee4c75398549e9e3b454fa27e845e05
monta0315 <monta0315@docomo.ne.jp>
2022-05-31 15:35:05 +0900

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [circleColor, setCircleColor] = React.useState("green");
+  const [start, setStart] = React.useState(false);
+  const [viewDim, setViewDim] = React.useState({ width: 0, height: 0 });
+  const [startTime, setStartTime] = React.useState(0);
+  const [result, setResult] = React.useState(0);
+
+  React.useEffect(() => {
+    const view_width = window.innerWidth;
+    const view_height = window.innerHeight;
+    setViewDim({ width: view_width, height: view_height });
+  }, []);
+
+  const onClickStart = () => {
+    setStart(true);
+    setResult(0);
+    setTimeout(() => {
+      setCircleColor("red");
+      const start = performance.now();
+      setStartTime(start);
+    }, 3000);
+  };
+
+  const onClickEnd = () => {
+    const end = performance.now();
+    setResult(end - startTime);
+    setCircleColor("green");
+    setStart(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="display">
+      <svg width={viewDim.width} height={viewDim.height / 2}>
+        <circle
+          r={30}
+          cx={viewDim.width / 2}
+          cy={viewDim.height / 3}
+          fill={circleColor}
+        />
+      </svg>
+      {start ? (
+        <button onClick={onClickEnd}>click!</button>
+      ) : (
+        <button onClick={onClickStart}>start!</button>
+      )}
+      <h1>{"time: " + result + "msec"}</h1>
     </div>
   );
-}
+};
 
 export default App;
